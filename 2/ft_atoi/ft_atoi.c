ed7615cb9e5f4e721a2b1a736f7fbd71a896d0ee
Burak Kozluca <bkozluca@k1m13s07.42kocaeli.com.tr>
2022-12-07 14:17:15 +0300

    ft_atoi

--- 2/ft_atoi/ft_atoi.c
+++ 2/ft_atoi/ft_atoi.c
@@ -1,19 +1,15 @@
-#include <unistd.h>
-
 int	ft_atoi(char *s)
 {
-	int	sign;
-	int	r;
+	int	sign = 1;
+	int	r = 0;
 
-	while (*s <= ' ' || (*s <= 13 && *s >= 9))
+	while (*s == ' ' || (*s <= 13 && *s >= 9))
 		s++;
-	if (*s == '-' || *s == '+')
-	{
-		if (*s == '-')
-			sign = -1;
+	if (*s == '-')
+		sign = -1;
+	if(*s == '+' || *s == '-')
 		s++;
-	}
-	while (*s >= '0' || *s <= '9')
+	while (*s >= '0' && *s <= '9')
 	{
 		r = r * 10 + *s - '0';
 		s++;
